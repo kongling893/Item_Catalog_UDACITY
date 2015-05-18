3beb2be79c1cc02f151dc263d3bcc958b2e6430e
kongling893 <kongling893@163.com>
2015-05-17 22:09:17 -0500

    added user table into database

--- database_setup.py
+++ database_setup.py
@@ -7,6 +7,14 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+	__tablename__ = 'user'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	email = Column(String(250), nullable=False)
+	picture = Column(String(250))
+
+
 
 class ToyShop(Base):
 	__tablename__ = 'toy_shop'
