bb46e689d181d7a32fdba3c1db4241340fff031f
kongling893 <kongling893@163.com>
2015-06-03 04:05:48 -0500

    realized function to create new shop

--- database_setup.py
+++ database_setup.py
@@ -15,7 +15,15 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
-
+    @property
+    def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'name': self.name,
+			'email': self.email,
+			'id': self.id,
+			'picture': self.picture,
+		}
 
 class ToyShop(Base):
 	__tablename__ = 'toy_shop'
@@ -32,6 +40,7 @@ class ToyShop(Base):
 			'name': self.name,
 			'description': self.description,
 			'id': self.id,
+			'user_id': self.user_id,
 		}
 
 class ToyItem(Base):
@@ -42,8 +51,8 @@ class ToyItem(Base):
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('toy_shop.id'))
-    restaurant = relationship(ToyShop)
+    shop_id = Column(Integer, ForeignKey('toy_shop.id'))
+    shop = relationship(ToyShop)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
