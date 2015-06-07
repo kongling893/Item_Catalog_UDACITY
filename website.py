e45b1ea487703d3e5f7d6ef12b764f995b511414
kongling893 <kongling893@163.com>
2015-06-07 08:36:23 -0500

    use json to represent credencials

--- website.py
+++ website.py
@@ -70,6 +70,7 @@ def gconnect():
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
+        
     except FlowExchangeError:
         response = make_response(json.dumps(
             'Failed to upgrade the authorization code.'
@@ -78,7 +79,16 @@ def gconnect():
         return response
 
     # Check that the access token is valid.
-    access_token = credentials.access_token
+	'''
+	to_json(self) 
+	Creating a JSON representation of an instance of Credentials.
+
+	Returns:
+	   string, a JSON representation of this instance, suitable to pass to
+	'''
+    credentials = credentials.to_json()             #After using to_json, we get a string.
+    credentials = json.loads(credentials)           # Get the corresponding dictionary.
+    access_token = credentials['token_response']['access_token']     #
     url = (
         'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
         % access_token)
@@ -91,7 +101,7 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
 
     # Verify that the access token is used for the intended user.
-    gplus_id = credentials.id_token['sub']
+    gplus_id = credentials['id_token']['sub']
     if result['user_id'] != gplus_id:
         response = make_response(
             json.dumps("Token's user ID doesn't match given user ID."), 401)
@@ -116,13 +126,11 @@ def gconnect():
 
     # Store the access token in the session for later use.
     login_session['provider'] = 'google'
-    login_session['credentials'] = credentials
-    login_session['gplus_id'] = gplus_id
     response = make_response(json.dumps('Successfully connected user.', 200))
 
     print "#Get user info"
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    params = {'access_token': credentials['token_response']['access_token'], 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
     data = json.loads(answer.text)
 
@@ -163,7 +171,7 @@ def gdisconnect():
             'Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    access_token = credentials.access_token
+    access_token = credentials['token_response']['access_token']
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
